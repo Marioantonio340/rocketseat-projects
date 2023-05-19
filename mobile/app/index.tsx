7d4f4aaef4b02cd7b8b837c590851fbebc0ca48f
Mario Antonio Fernandes de Oliveira <43358964+Marioantonio340@users.noreply.github.com>
2023-05-19 16:56:59 -0300

    feat(mobile): definido layout de rotas, pagina de nova memoria

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,19 +1,12 @@
-import { StatusBar } from 'expo-status-bar'
-import { ImageBackground, Text, View, TouchableOpacity } from 'react-native'
+import { Text, View, TouchableOpacity } from 'react-native'
 import * as SecureStore from 'expo-secure-store'
 import { styled } from 'nativewind'
 import { makeRedirectUri, useAuthRequest } from 'expo-auth-session'
 import { useEffect } from 'react'
 import { useRouter } from 'expo-router'
 
-import {
-  useFonts,
-  Roboto_400Regular,
-  Roboto_700Bold
-} from '@expo-google-fonts/roboto'
-import { BaiJamjuree_700Bold } from '@expo-google-fonts/bai-jamjuree'
 
-import blurBg from '../src/assets/bg-blur.png'
+
 import Stripes from '../src/assets/stripes.svg'
 import NlwLogo from '../src/assets/nlw-spacetime-logo.svg'
 import { api } from '../src/lib/api'
@@ -30,11 +23,6 @@ const discovery = {
 export default function App() {
   const router = useRouter()
 
-  const [fontsLoaded] = useFonts({
-    Roboto_400Regular,
-    Roboto_700Bold,
-    BaiJamjuree_700Bold
-  })
 
 
   const [request, response, signInWithGithub] = useAuthRequest(
@@ -68,19 +56,9 @@ export default function App() {
   }, [response])
 
 
-  if (!fontsLoaded) {
-    return null
-  }
+
   return (
-    <ImageBackground
-      source={blurBg}
-      className="bg-gray-950 flex-1 px-8 py-10"
-      imageStyle={{
-        position: 'absolute',
-        left: '-100%',
-      }}
-    >
-      <StyledStripes className='absolute left-2' />
+    <View className=" flex-1 px-8 py-10 items-center">
 
       <View className='flex-1 items-center justify-center gap-6'>
         <NlwLogo />
@@ -111,7 +89,7 @@ export default function App() {
       <Text className="text-center font-body text-sm leading-relaxed text-gray-200" >
         Feito com 💜 no NLW da Rocketseat
         </Text>
-      <StatusBar style="light" translucent />
-    </ImageBackground>
+
+    </View>
   )
 }
